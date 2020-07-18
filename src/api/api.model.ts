45dd8d248939fcd3b0b685af415b41d12fc0ec90
Erik Szabó <erik1szabo1@gmail.com>
2020-07-18 12:21:32 +0200

    fix heroku

--- src/api/api.model.ts
+++ src/api/api.model.ts
@@ -1,6 +1,6 @@
 import monk from 'monk';
 
-export const db = monk(process.env.MONGO_URI || '127.0.0.1:27017/devrik-api');
+export const db = monk(process.env.MONGODB_URI || '127.0.0.1:27017/devrik-api');
 
 export const projectsPreview = db.get('projects-preview');
 export const projects = db.get('projects');
